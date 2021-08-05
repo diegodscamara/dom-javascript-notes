3be0597c276db7fb80634603bf5b48d8649282f6
Your Namegit <you@example.com>
2021-08-05 14:16:50 -0300

    toggle, parentElement and IIFE

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,24 +1,45 @@
-const criarTarefa = (evento) => {
+(() => {
+    const criarTarefa = (evento) => {
 
-    evento.preventDefault()
+        evento.preventDefault()
 
-    const lista = document.querySelector('[data-task]')
-    const input = document.querySelector('[data-form-input]')
-    const valor = input.value
+        const lista = document.querySelector('[data-task]')
+        const input = document.querySelector('[data-form-input]')
+        const valor = input.value
 
-    const tarefa = document.createElement('li')
-    tarefa.classList.add('task')
+        const tarefa = document.createElement('li')
+        tarefa.classList.add('task')
 
-    const conteudo = `<p class="content">${valor}</p>`
+        const conteudo = `<p class="content">${valor}</p>`
 
-    tarefa.innerHTML = conteudo
+        tarefa.innerHTML = conteudo
 
-    lista.appendChild(tarefa)
+        tarefa.appendChild(BotaoConclui())
+        lista.appendChild(tarefa)
 
-    input.value = ""
+        input.value = ""
 
-}
+    }
 
-const novaTarefa = document.querySelector('[data-form-button]')
+    const novaTarefa = document.querySelector('[data-form-button]')
 
-novaTarefa.addEventListener('click', criarTarefa)
+    novaTarefa.addEventListener('click', criarTarefa)
+
+    const BotaoConclui = () => {
+        const botaoConclui = document.createElement('button')
+
+        botaoConclui.classList.add('check-button')
+        botaoConclui.innerText = 'concluir'
+        botaoConclui.addEventListener('click', concluirTarefa)
+
+        return botaoConclui
+    }
+
+    const concluirTarefa = (evento) => {
+        const botaoConclui = evento.target
+
+        const tarefaCompleta = botaoConclui.parentElement
+
+        tarefaCompleta.classList.toggle('done')
+    }
+})()
